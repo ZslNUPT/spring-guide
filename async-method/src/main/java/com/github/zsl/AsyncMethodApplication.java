381b72962f7e329c99eaeac07552ec55a2acfeb5
ZuoSL <1024958871@qq.com>
2017-01-17 16:04:48 +0800

    async-method

---
+++ async-method/src/main/java/com/github/zsl/AsyncMethodApplication.java
@@ -0,0 +1,30 @@
+package com.github.zsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@SpringBootApplication
+@EnableAsync
+public class AsyncMethodApplication extends AsyncConfigurerSupport {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncMethodApplication.class, args);
+	}
+
+	@Override
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("GithubLookup-");
+		executor.initialize();
+		return executor;
+	}
+
+}
